adcc10bbcf55b4e82077049881990e6d75a7cff5
dreamwjj <1361274404@qq.com>
2022-02-06 14:50:46 +0800

    20220206学习笔记

--- Mybatis-Study/mybatis-01/src/main/java/cn/itcast/dao/UserDao.java
+++ Mybatis-Study/mybatis-01/src/main/java/cn/itcast/dao/UserDao.java
@@ -3,7 +3,19 @@ package cn.itcast.dao;
 import cn.itcast.pojo.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserDao {
+    //获取所有用户
     List<User> getUserList();
+    //根据id查询用户
+    User getUserById(int id);
+    //插入用户
+    int addUser(User user);
+    //插入用户2
+    int addUser2(Map<String,Object> usermap);
+    //修改用户
+    int updateUser(User user);
+    //删除用户
+    int deleteUser(int id);
 }
--- Mybatis-Study/mybatis-01/src/test/java/cn/itcast/dao/UserDaoTest.java
+++ Mybatis-Study/mybatis-01/src/test/java/cn/itcast/dao/UserDaoTest.java
@@ -5,7 +5,9 @@ import cn.itcast.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserDaoTest {
     @Test
@@ -16,6 +18,58 @@ public class UserDaoTest {
         for (User user : userList) {
             System.out.println(user);
         }
+        sqlSession.close();
+    }
+
+    @Test
+    public void test01(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserDao userMapper = sqlSession.getMapper(UserDao.class);
+        User user = userMapper.getUserById(6);
+        System.out.println(user);
+        sqlSession.close();
+    }
+    @Test
+    public void test02(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserDao mapper = sqlSession.getMapper(UserDao.class);
+        int i = mapper.addUser(new User
+                (1, 123456, "wqp", "ly", "男",
+                        8, "江苏", "1361274404", "123321@qq.com"));
+        if (i>0){
+            System.out.println("插入成功");
+        }
+        sqlSession.commit();
+        sqlSession.close();
+    }
+    @Test
+    public void test03(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserDao mapper = sqlSession.getMapper(UserDao.class);
+        mapper.updateUser(new User
+                (1, 123456, "wjj", "wjj", "男",
+                        8, "江苏", "1361274404", "123321@qq.com"));
+        sqlSession.commit();
+        sqlSession.close();
+    }
+    @Test
+    public void test04(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserDao mapper = sqlSession.getMapper(UserDao.class);
+        mapper.deleteUser(1);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+    @Test
+    public void test05(){
+        final SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserDao mapper = sqlSession.getMapper(UserDao.class);
+        Map<String,Object> map = new HashMap<String, Object>();
+        map.put("id",1);
+        map.put("username","wjj");
+        map.put("password",123456);
+        mapper.addUser2(map);
+        sqlSession.commit();
         sqlSession.close();
     }
 }
